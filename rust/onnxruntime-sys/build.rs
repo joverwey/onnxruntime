3bef86aa6cd575ed117d8eadaa6795960f05f429
Johan Verwey <joverwey@microsoft.com>
2020-07-28 09:39:19 -0700

    refactor to make changing version easy in the future

--- rust/onnxruntime-sys/build.rs
+++ rust/onnxruntime-sys/build.rs
@@ -5,22 +5,45 @@ use std::env;
 use std::path::PathBuf;
 
 fn download_dependencies() -> Result<PathBuf, ExtractError> {
-    let (path, folder) = if cfg!(target_os = "windows") {
-        ("https://github.com/microsoft/onnxruntime/releases/download/v1.4.0/onnxruntime-win-x64-1.4.0.zip", "onnxruntime-win-x64-1.4.0")
+    let version = "1.4.0";
+
+    let (platform, extension) = if cfg!(target_os = "windows") {
+        ("win", "zip")
     } else if cfg!(target_os = "linux") {
-        ("https://github.com/microsoft/onnxruntime/releases/download/v1.4.0/onnxruntime-linux-x64-1.4.0.tgz", "onnxruntime-linux-x64-1.4.0")
+        ("linux", "tgz")
     } else if cfg!(target_os = "macos") {
-        ("https://github.com/microsoft/onnxruntime/releases/download/v1.4.0/onnxruntime-osx-x64-1.4.0.tgz", "onnxruntime-osx-x64-1.4.0")
+        ("osx", "tgz")
     } else {
         panic!("Unsupported platform")
     };
 
-    let path_to_lib_zip = download_or_find_file(path.into(), None).expect(&format!(
-        "Unable to download pre-compiled binaries from '{}'",
-        path
-    ));
+    let folder = format!(
+        "onnxruntime-{platform}-x64-{version}",
+        version = version,
+        platform = platform
+    );
+    let archive_file = format!("onnxruntime-{}-x64-{}.{}", platform, version, extension);
+    let path = format!(
+        "https://github.com/microsoft/onnxruntime/releases/download/v{}/{}",
+        version, archive_file
+    );
 
-    let path_to_extracted_files = extract_archive(&path_to_lib_zip, None)?.join(folder);
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let archive_target_file = out_path.join(archive_file);
+
+    let path_to_lib_zip = if !out_path.exists() {
+        download_or_find_file(&path, None).expect(&format!(
+            "Unable to download pre-compiled binaries from '{}'",
+            path
+        ))
+    } else {
+        archive_target_file
+    };
+
+    let path_to_extracted_files = out_path.join(&folder);
+    if !path_to_extracted_files.exists() {
+        extract_archive(&path_to_lib_zip, None)?;
+    }
 
     Ok(path_to_extracted_files)
 }
@@ -46,13 +69,6 @@ fn main() {
         .expect("Failed to install dynamic libraries");
 
     add_link_search_path(&path_to_dylib_folder);
-    // if cfg!(windows) {
-    //     println!("cargo:rustc-link-search=../../build/Windows/RelWithDebInfo/RelWithDebInfo");
-    //     println!("cargo:rustc-link-search=build/Windows/RelWithDebInfo/RelWithDebInfo");
-    // } else {
-    //     println!("cargo:rustc-link-search=../../build/Linux/RelWithDebInfo");
-    //     println!("cargo:rustc-link-search=build/Linux/RelWithDebInfo");
-    // }
 
     println!("cargo:rustc-link-lib=dylib=onnxruntime");
     let bindings = bindgen::Builder::default()
